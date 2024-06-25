a1cbc16a68d332803078f7027ae8131acd175571
Jeong-yuchan <juh5020@naver.com>
2024-06-25 17:21:42 +0900

    Add files via upload

---
+++ CSharpBasic2/TriangleArea.cs
@@ -0,0 +1,19 @@
+
+namespace ConsoleApp5
+{
+    internal class TriangleArea
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("가로를 입력하세요 : ");
+            int width = int.Parse(Console.ReadLine());
+            Console.Write("세로를 입력하세요 : ");
+            int height = int.Parse(Console.ReadLine());
+
+            double result = (width * height)/2.0; // 수식
+            
+            Console.WriteLine($"넓이는 {result} 입니다.");
+            
+        }
+    }
+}
